147adf14e53c1ac19fda2e2c85668cb14475eef1
Yang Li <yang.li@crypto.com>
2020-12-30 14:20:21 +0800

    Add graph data

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,84 +1,58 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts"
 import {
-  Pair,
-  Approval,
-  Burn,
-  Mint,
-  Swap,
-  Sync,
-  Transfer
+  Transfer,
+  Pair
 } from "../generated/Pair/Pair"
-import { ExampleEntity } from "../generated/schema"
+import { TransferEntity } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+let ZERO_BI = BigInt.fromI32(0)
+let ONE_BI = BigInt.fromI32(1)
+let BI_18 = BigInt.fromI32(18)
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+function exponentToBigDecimal(decimals: BigInt): BigDecimal {
+  let bd = BigDecimal.fromString('1')
+  for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
+    bd = bd.times(BigDecimal.fromString('10'))
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.MINIMUM_LIQUIDITY(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.burn(...)
-  // - contract.decimals(...)
-  // - contract.factory(...)
-  // - contract.getReserves(...)
-  // - contract.kLast(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.price0CumulativeLast(...)
-  // - contract.price1CumulativeLast(...)
-  // - contract.symbol(...)
-  // - contract.token0(...)
-  // - contract.token1(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+  return bd
 }
 
-export function handleBurn(event: Burn): void {}
+function convertTokenToDecimal(tokenAmount: BigInt, exchangeDecimals: BigInt): BigDecimal {
+  if (exchangeDecimals == ZERO_BI) {
+    return tokenAmount.toBigDecimal()
+  }
+  return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals))
+}
 
-export function handleMint(event: Mint): void {}
+export function handleTransfer(event: Transfer): void {
+  let hash = event.transaction.hash.toHexString();
 
-export function handleSwap(event: Swap): void {}
+  let thisTime = 0;
 
-export function handleSync(event: Sync): void {}
+  for (let i = 0; i < 1000; i++) {
+    let exists = TransferEntity.load(hash + '-' + i.toString())
 
-export function handleTransfer(event: Transfer): void {}
+    if (!exists) {
+      thisTime = i;
+      break;
+    }
+  }
+
+  let pairContract = Pair.bind(event.address)
+
+  let entity = new TransferEntity(hash + '-' + thisTime.toString());
+
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+
+  let balanceOf = pairContract.balanceOf(entity.to as Address);
+
+  entity.contractAddress = event.address;
+  entity.rawAmount = event.params.value.toBigDecimal();
+  entity.rawBalanceOf = balanceOf.toBigDecimal();
+  entity.amount = convertTokenToDecimal(balanceOf, BI_18);
+
+  entity.blockNumber = event.block.number
+
+  entity.save()
+}
